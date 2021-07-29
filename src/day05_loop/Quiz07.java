38690dccbbdec62b7c52f7ac6801c221331e88fc
hyeonwoo <ejffoddl1@naver.com>
2021-07-29 11:19:19 +0900

    Quiz07 풀이

---
+++ src/day05_loop/Quiz07.java
@@ -0,0 +1,22 @@
+package day05_loop;
+
+public class Quiz07 {
+	public static void main(String[] args) {
+		for (int i=1; i<5; i++) {
+			System.out.println(i + ". hello");
+		}
+		int sum=0;
+		for(int i=1; i<=100; i++) {
+			sum+=i;
+			if(sum == 528) {
+				System.out.println(i);
+			}
+		}
+		for(int i=1; i<=25; i++) {
+			System.out.print(i + "\t");
+			if(i%5==0) {
+				System.out.println();
+			}
+		}
+	}
+}
